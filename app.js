52c1155860dccc37d36b54be1f66218a6b5cadc5
Darren Porciello <darren.porciello1@gmail.com>
2023-09-26 11:00:52 -0400

    Added ejsMate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Trail = require('./models/trail');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://127.0.0.1:27017/local-trails', { useNewUrlParser: true, useUnifiedTopology: true})
 
@@ -16,6 +17,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
